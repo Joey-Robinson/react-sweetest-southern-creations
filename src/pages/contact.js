c873ff4590678ecb331715bd809ba5f2afa23567
Joey Robinson <theamazingness@gmail.com>
2019-06-13 01:12:22 -0400

    Added react-icons for contact page. Will finish up tomorrow

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaFacebook } from "react-icons/fa/index.esm"
+import { FaInstagram } from "react-icons/fa/index.esm"
+import { FaPinterest } from "react-icons/fa/index.esm"
+import { FaGooglePlusG } from "react-icons/fa/index.esm"
 
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
     <section className="contact">
-      <h1>from contact</h1>
+      <h1>Get in touch!</h1>
+      <div className="contact--fb">
+        <a href="">
+          Facebook - <FaFacebook aria-label="Facebook" />
+        </a>
+      </div>
+      <div className="contact--ig">
+        <a href="">
+          Instagram - <FaInstagram aria-label="Instagram" />
+        </a>
+      </div>
+      <div className="contact--pint">
+        <a href="">
+          Pinterest - <FaPinterest aria-label="Pinterest" />
+        </a>
+      </div>
+      <div className="contact--email">
+        <a href="">
+          Email - <FaGooglePlusG aria-label="Gmail" />
+        </a>
+      </div>
     </section>
   </Layout>
 )
